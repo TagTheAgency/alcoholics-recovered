ecebe3f2625c65fe57d156f51f3e0fe7d4c58137
Rebecca Leete <rebecca072@live.com>
2018-11-19 14:47:24 +1300

    add drop down top nav menu

--- navBarOverlay.js
+++ navBarOverlay.js
@@ -1,6 +1,4 @@
-// var sidebar = document.getElementById("sidebar-overlay")
-
-// console.log(sidebar)
+// Sidebar Nav ./process.html
 
 function openNav() {
   document.getElementById("sidebar-overlay").style.width = "200px";
@@ -10,4 +8,17 @@ function openNav() {
 function closeNav() {
   document.getElementById("sidebar-overlay").style.width = "0";
   document.getElementById("sidebar-hamburger-icon-container").style.display = "block";
+}
+
+// Top Nav site header
+
+function openTopNav() {
+  document.getElementById("top-nav").style.height = "65%";
+  console.log("topNav open!")
+}
+
+/* Close */
+function closeTopNav() {
+  document.getElementById("top-nav").style.height = "0";
+  console.log("topNav closed!")
 }
